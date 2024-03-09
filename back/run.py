cef1aaed01bab3ee2cac4444cd5e1c91905270bd
Daemon2017 <daemon2017@gmail.com>
2024-03-09 15:04:23 +0300

    Исправлена ошибка

--- back/run.py
+++ back/run.py
@@ -13,6 +13,7 @@ cors = CORS(app)
 @app.route('/measure', methods=['POST'])
 def measure():
     df = utils.get_df(request.data)
+    df = df[~df.index.duplicated(keep='first')]
     df = utils.get_solved_na_df_1(df)
     df = utils.get_solved_duplications_df(df)
     df = df.dropna(axis=1, how='all')
